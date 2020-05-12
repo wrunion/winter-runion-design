89f2d14166139b32673c07367f568e31b4167488
wrunion <winterrunion@gmail.com>
2020-05-12 14:02:57 -0700

    Add box-shadow styling.

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -6,6 +6,8 @@ function AboutText() {
       <h1>Winter A. Runion</h1>
       <h2 className="subheader">Web Dev & Software Engineering</h2>
       <h4>Portland, Oregon</h4>
+      <p>Github | LinkedIn</p>
+      <h4 id="email"><em>Contact: winter@winterrunion.com</em></h4>
     </div>
   )
 }
