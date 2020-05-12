8796bac9cc83bb9edbf04615ce212a457fc175ad
wrunion <winterrunion@gmail.com>
2020-05-12 13:19:26 -0700

    Create AboutText component.

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import avatar from './meRound500.png';
 import './About.css';
+import AboutText from './AboutText';
 
 function About() {
   return (
     <div className="About">
       <img src={avatar} alt="" />
+      <AboutText />
     </div>
   );
 }
---
+++ src/components/AboutText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutText() {
+  return (
+    <div className="AboutText">
+      <p>Test test</p>
+    </div>
+  )
+}
+
+export default AboutText;
