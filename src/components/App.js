780b421d6becdf420b7af84e98ba8ffa53aedef7
wrunion <winterrunion@gmail.com>
2020-05-12 11:46:38 -0700

    Initial commit.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container App">
+      <h1>Header Here</h1>
+    </div>
+  );
+}
+
+export default App;
