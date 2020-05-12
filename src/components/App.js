316442088caad00e852c4e66db1a1b5dd4ca87d0
wrunion <winterrunion@gmail.com>
2020-05-12 11:56:17 -0700

    Create Polaroid and ImageList components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ImageList from './Polaroid/ImageList';
 
 function App() {
   return (
     <div className="container App">
       <h1>Header Here</h1>
+      <ImageList />
     </div>
   );
 }
---
+++ src/components/Polaroid/ImageList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ImageList() {
+  return (
+    <div>
+      <h3>[Image List Goes Here]</h3>
+    </div>
+  )
+}
+
+export default ImageList;
