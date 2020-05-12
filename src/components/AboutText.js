d5293338aa5622718015743bfd43b51dbfccc672
wrunion <winterrunion@gmail.com>
2020-05-12 14:43:30 -0700

    Finish adding contact links.

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -9,8 +9,8 @@ function AboutText() {
       </div>
       <p className="location">Portland, Oregon</p>
       {/* <div><button type="button">Github</button> <button type="button">LinkedIn</button></div> */}
-      <p className="links"><a href="https://github.com/wrunion" target="_blank" rel="noopener noreferrer">Github</a> | <a href="https://github.com/wrunion" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-      <p id="email"><em>Contact: winter@winterrunion.com</em></p>
+      <p className="links"><a href="https://github.com/wrunion" target="_blank" rel="noopener noreferrer">Github</a> | <a href="https://www.linkedin.com/in/wrunion/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+      <p id="email"><em><a href="mailto:winter@winterrunion.com?subject=Web Development">Contact: winter@winterrunion.com</a></em></p>
     </div>
   )
 }
