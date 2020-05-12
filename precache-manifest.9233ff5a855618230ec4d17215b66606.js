52b5e53bb2529e818d060c5bfd20e7d0c22f645b
wrunion <winterrunion@gmail.com>
2020-05-12 15:07:00 -0700

    Updates

---
+++ precache-manifest.9233ff5a855618230ec4d17215b66606.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ed83bdec41ca808591c5668c8f66b0",
+    "url": "/winter-runion-design/index.html"
+  },
+  {
+    "revision": "a59a0b7d050514299c42",
+    "url": "/winter-runion-design/static/css/main.32b21b4c.chunk.css"
+  },
+  {
+    "revision": "571cc965e10b6eefa155",
+    "url": "/winter-runion-design/static/js/2.3f151350.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/winter-runion-design/static/js/2.3f151350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59a0b7d050514299c42",
+    "url": "/winter-runion-design/static/js/main.b4a4b26c.chunk.js"
+  },
+  {
+    "revision": "da8d2be0f2e84c535b44",
+    "url": "/winter-runion-design/static/js/runtime-main.3832d7de.js"
+  },
+  {
+    "revision": "e11d3859d13f1513a97698f63b568a1b",
+    "url": "/winter-runion-design/static/media/meRound500.e11d3859.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/winter-runion-design/precache-manifest.9233ff5a855618230ec4d17215b66606.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/winter-runion-design/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
