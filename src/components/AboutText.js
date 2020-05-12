818a30f2d91c9351c8f15e9b3f1815a013a6429c
wrunion <winterrunion@gmail.com>
2020-05-12 14:38:27 -0700

    Add links.

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -3,11 +3,14 @@ import React from 'react';
 function AboutText() {
   return (
     <div className="AboutText">
-      <h1>Winter A. Runion</h1>
-      <h2 className="subheader">Web Dev & Software Engineering</h2>
-      <h4>Portland, Oregon</h4>
-      <p>Github | LinkedIn</p>
-      <h4 id="email"><em>Contact: winter@winterrunion.com</em></h4>
+      <div className="header">
+        <h1>Winter A. Runion</h1>
+        <h2 className="subheader">Web Dev & Software Engineering</h2>
+      </div>
+      <p className="location">Portland, Oregon</p>
+      {/* <div><button type="button">Github</button> <button type="button">LinkedIn</button></div> */}
+      <p className="links"><a href="https://github.com/wrunion" target="_blank" rel="noopener noreferrer">Github</a> | <a href="https://github.com/wrunion" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+      <p id="email"><em>Contact: winter@winterrunion.com</em></p>
     </div>
   )
 }
