76a3fbce27bb435451cf489ebb6a0a830b4066da
wrunion <winterrunion@gmail.com>
2020-05-12 21:05:51 -0700

    ADd media queries for mobile sizes.

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import avatar from './meRound500.png';
 import './About.css';
-import AboutText from './AboutText';
+// import AboutText from './AboutText';
 
 function About() {
   return (
     <div className="About">
       <img src={avatar} alt="" />
-      <AboutText />
+      {/* <AboutText /> */}
+      <div className="AboutText">
+        <div className="header">
+          <h1>Winter A. Runion</h1>
+          <h2 className="subheader">Web Dev & Software Engineering</h2>
+        </div>
+        <p className="location">Portland, Oregon</p>
+        {/* <div><button type="button">Github</button> <button type="button">LinkedIn</button></div> */}
+        <p className="links"><a href="https://github.com/wrunion" target="_blank" rel="noopener noreferrer">Github</a> | <a href="https://www.linkedin.com/in/wrunion/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+        <p id="email"><em><a href="mailto:winter@winterrunion.com?subject=Web Development"> winter@winterrunion.com</a></em></p>
+      </div>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import './About.css';
-// import ImageList from './Polaroid/ImageList';
 import About from './About';
 
 function App() {
   return (
     <div className="container App">
-      {/* <h1>Header Here</h1> */}
-      {/* <ImageList /> */}
       <About />
     </div>
   );
